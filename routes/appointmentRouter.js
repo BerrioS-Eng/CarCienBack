f2a7b4b5a6ced559d8e7d2e4691341cddc9631af
Sebastian Berrio <sberrio09@gmail.com>
2024-10-29 09:55:34 -0500

    config workspace

--- routes/appointmentRouter.js
+++ routes/appointmentRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-import Appointment from '../controllers/appointmentController.js';
+import { getAll } from '../controllers/appointmentController.js';
 
 const appointmentRoute = express.Router();
 
-appointmentRoute.get('/all', Appointment);
-
+appointmentRoute.get('/all', getAll);
+/*
 appointmentRoute.get('/single/:id', Appointment);
 
 appointmentRoute.post('/create', Appointment);
@@ -12,6 +12,6 @@ appointmentRoute.post('/create', Appointment);
 appointmentRoute.put('/update/:id', Appointment);
 
 appointmentRoute.delete('/delete/:id', Appointment);
-
+*/
 
 export default appointmentRoute;
