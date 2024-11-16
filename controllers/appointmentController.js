6842ce46b0e92ba472d7389c937717de589e7128
Sebastian Berrio <sberrio09@gmail.com>
2024-11-16 12:33:32 -0500

    add methods query

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -4,6 +4,7 @@ import Appointment from '../models/appointmentModel.js';
 import { createAppointmentSchema } from '../middlewares/validators.js';
 
 export async function getAll(req, res) {
+    /*
     const { page } = req.query;
     const appointmentPerPage = 10;
 
@@ -20,6 +21,9 @@ export async function getAll(req, res) {
                                             .skip(pageNum * appointmentPerPage)
                                             .limit(appointmentPerPage)
                                             .populate({ path: "UserId", select: "email" });
+        */
+    try{
+        const result = await Appointment.find({});
 
         res.status(200).json({ success: true, message: "Appointment", data: result });
     } catch (error) {
@@ -54,11 +58,8 @@ export async function getOne(req, res) {
     const { _id } = req.query;
 
     try {
-        const existingAppointment = await Appointment.findOne({ _id }).populate({
-            path: "UserId",
-            select: "email"
-        })
-
+        const existingAppointment = await Appointment.findOne({ userId: _id }).exec();
+        
         if (!existingAppointment) {
             return res.status(404)
                         .json({ success: false, message: "Appointment unavailable"});
@@ -72,3 +73,58 @@ export async function getOne(req, res) {
     }
 };
 
+export async function updateAppointment (req, res) {
+    const { _id } = req.query;
+    const { service, detail, appointmentDate } = req.body;
+    const { userId } = req.user;
+
+    try {
+        const { error, value } = createAppointmentSchema.validate({ service, detail, appointmentDate, userId });
+        if (error) {
+            return res.status(401)
+                        .json({ success: false, message: error.details[0].message });
+        }
+        const existingAppointment = await Appointment.findOne({ _id });
+        
+        if (!existingAppointment) {
+            return res.status(404)
+                        .json({ success: false, message: "Appointment unavailable"});
+        }
+        if (existingAppointment.userId.toString() !== userId) {
+            return res.status(403)
+                        .json({ success: false, message: "Unauthorized"});
+        }
+
+        existingAppointment.service = service;
+        existingAppointment.detail = detail;
+        existingAppointment.appointmentDate = appointmentDate;
+
+        const result = await existingAppointment.save();
+        res.status(200).json({ success: true, message: "Update", data: result });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function deleteAppointment (req, res) {
+    const { _id } = req.query;
+	const { userId } = req.user;
+	try {
+		const existingAppointment = await Appointment.findOne({ _id });
+        
+		if (!existingAppointment) {
+			return res.status(404)
+				.json({ success: false, message: 'Appointment already unavailable' });
+		}
+		if (existingAppointment.userId.toString() !== userId) {
+			return res.status(403)
+                    .json({ success: false, message: 'Unauthorized' });
+		}
+
+		await Appointment.deleteOne({ _id });
+		res.status(200).json({ success: true, message: 'deleted' });
+	} catch (error) {
+		console.log(error);
+	}
+}
--- routes/appointmentRouter.js
+++ routes/appointmentRouter.js
@@ -1,17 +1,18 @@
 import express from "express";
-import { getAll, create } from '../controllers/appointmentController.js';
+import { identifier } from "../middlewares/identification.js";
+import { getAll, create, getOne, updateAppointment, deleteAppointment } from '../controllers/appointmentController.js';
 
 const appointmentRoute = express.Router();
 
 appointmentRoute.get('/all', getAll);
 
-appointmentRoute.get('/single/:id', Appointment);
+appointmentRoute.get('/single-post', getOne);
 
-appointmentRoute.post('/create', create);
-/*
-appointmentRoute.put('/update/:id', Appointment);
+appointmentRoute.post('/create', identifier, create);
 
-appointmentRoute.delete('/delete/:id', Appointment);
-*/
+appointmentRoute.put('/update', identifier, updateAppointment);
+
+appointmentRoute.delete('/delete', identifier, deleteAppointment);
+
 
 export default appointmentRoute;
