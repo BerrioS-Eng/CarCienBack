0a62e33a5b6ac40d5ba6fe96d73f7fef0b3ddfd0
ElKev118 <kevin.ruda@udea.edu.co>
2024-11-17 00:12:40 -0500

    funcionalidad citas sin usuario logueado

--- middlewares/identification.js
+++ middlewares/identification.js
@@ -1,28 +1,28 @@
 import jwt from "jsonwebtoken";
 
 export const identifier = (req, res, next) => {
-    let token;
-    if(req.headers.client === "not-browser"){
-        token = req.headers.authorization
+  let token;
+  if (req.headers.client === "not-browser") {
+    token = req.headers.authorization;
+  } else {
+    token = req.cookies["Authorization"];
+  }
+
+  if (!token) {
+    return res.status(403).json({ success: false, message: "Unauthorized" });
+  }
+
+  try {
+    const userToken = token.split(" ")[1];
+    const jwtVerified = jwt.verify(userToken, process.env.SECRET_KEY);
+    if (jwtVerified) {
+      req.user = jwtVerified;
+      next();
     } else {
-        token = req.cookies["Authorization"]
+      throw new Error("Error in token");
     }
-
-    if(!token) {
-        return res.status(403)
-                    .json({ success: false, message: "Unauthorization"});
-    }
-
-    try {
-        const userToken = token.split(' ')[1];
-        const jwtVerified = jwt.verify(userToken, process.env.SECRET_KEY);
-        if(jwtVerified){
-            req.user = jwtVerified;
-            next();
-        } else {
-            throw new Error('Error in token');
-        }
-    } catch (error) {
-        console.log(error)
-    }
-}
+  } catch (error) {
+    console.log(error);
+    return res.status(403).json({ success: false, message: "Unauthorized" });
+  }
+};
--- routes/appointmentRouter.js
+++ routes/appointmentRouter.js
@@ -1,18 +1,24 @@
 import express from "express";
 import { identifier } from "../middlewares/identification.js";
-import { getAll, create, getOne, updateAppointment, deleteAppointment } from '../controllers/appointmentController.js';
+import {
+  getAll,
+  create,
+  getOne,
+  updateAppointment,
+  deleteAppointment,
+} from "../controllers/appointmentController.js";
 
 const appointmentRoute = express.Router();
 
-appointmentRoute.get('/all', getAll);
+appointmentRoute.get("/all", getAll);
 
-appointmentRoute.get('/single-post', getOne);
+appointmentRoute.get("/single-post", getOne);
 
-appointmentRoute.post('/create', identifier, create);
+//appointmentRoute.post("/create", identifier, create);
+appointmentRoute.post("/create", create);
 
-appointmentRoute.put('/update', identifier, updateAppointment);
+appointmentRoute.put("/update", identifier, updateAppointment);
 
-appointmentRoute.delete('/delete', identifier, deleteAppointment);
-
+appointmentRoute.delete("/delete", identifier, deleteAppointment);
 
 export default appointmentRoute;
